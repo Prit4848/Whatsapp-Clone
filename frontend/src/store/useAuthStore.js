9509d5447b7866c2733db5407a7a10c5e611aed1
Prit <pritrajput415@gmail.com>
2026-03-10 16:56:14 +0530

    update frontecors origin

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import  axiosInstance  from "../services/axiosInstance";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? import.meta.env.VITE_BASE_URI : "/";
+const BASE_URL = "https://whatsapp-clone-c57y.onrender.com";
 
 console.log("BASE_URL",BASE_URL);
 
